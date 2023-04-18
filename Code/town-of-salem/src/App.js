f0a92be508d639a06096e17d92ecb37bb71cec08
RARES129 <dascaluraresm@gmail.com>
2023-04-18 20:22:22 +0300

    Creation of the InfoAlert component that displays the remaining time depending on the state of the game, displays during the night what the respective role must do, displays when the voting ends and when the night begins.

--- Code/town-of-salem/src/App.js
+++ Code/town-of-salem/src/App.js
@@ -1,34 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import UserList from './components/UserList.js';
-import Action from './components/Action.js';
+import logo from "./logo.svg";
+import "./App.css";
+import UserList from "./components/UserList.js";
+import Action from "./components/Action.js";
 import { useEffect, useState } from "react";
+import InfoAlert from "./components/InfoAlert";
 
-function makeUser()
-{
+function makeUser() {
   let user = {
-    userName : "Username" + Math.floor(Math.random() * 100),
-    isAlive : ( (Math.floor(Math.random() * 2)) == 1 ) ? false : true,
-    id : Math.floor(Math.random() * 10000)
-  }
-  return user; 
+    userName: "Username" + Math.floor(Math.random() * 100),
+    isAlive: Math.floor(Math.random() * 2) == 1 ? false : true,
+    id: Math.floor(Math.random() * 10000),
+  };
+  return user;
 }
 
 let usersList = [];
-for (let i = 0; i < 20; i++)
-{
+for (let i = 0; i < 20; i++) {
   usersList.push(makeUser());
 }
 
 let currentUser = {
   roleName: "doctor",
   actionText: "HEAL",
-  nrOfSelection: 1
+  nrOfSelection: 1,
 };
 
 let currentGameState = {
-  state : "voting",
-  time : 30
+  state: "voting",
+  time: 30,
 };
 
 function App() {
@@ -37,43 +36,53 @@ function App() {
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      if (timeLeft > 0)
-        setTimeLeft(timeLeft - 1);
-      else
-      {
+      if (timeLeft > 0) setTimeLeft(timeLeft - 1);
+      else {
         clearTimeout(timer);
         let nextGameState;
-        if (gameState.state == "voting")
-        {
+        if (gameState.state == "voting") {
           nextGameState = {
-            state : "night",
-            time : 45
+            state: "votingEnd",
+            time: 5,
           };
-        }
-        else
-        {
+        } else if (gameState.state == "votingEnd") {
           nextGameState = {
-            state : "voting",
-            time : 30
+            state: "nightStart",
+            time: 5,
+          };
+        } else if (gameState.state == "nightStart") {
+          nextGameState = {
+            state: "night",
+            time: 45,
+          };
+        } else {
+          nextGameState = {
+            state: "voting",
+            time: 30,
           };
         }
 
         setGameState(nextGameState);
         setTimeLeft(nextGameState.time);
       }
-
     }, 1000);
 
     return () => clearTimeout(timer);
   });
 
   return (
-    <div className='app'>
-      <h1>State: {gameState.state} Remaining time {timeLeft}</h1>
-      <UserList
-        usersList = {usersList} gameState = {gameState} currentUser = {currentUser}
+    <div className="app">
+      <InfoAlert
+        gameState={gameState}
+        timeLeft={timeLeft}
+        currentUser={currentUser}
       />
-      <Action gameState = {gameState} currentUser = {currentUser} />
+      <UserList
+        usersList={usersList}
+        gameState={gameState}
+        currentUser={currentUser}
+      />
+      <Action gameState={gameState} currentUser={currentUser} />
     </div>
   );
 }
