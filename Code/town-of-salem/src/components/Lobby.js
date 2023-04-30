a07120446e25778f4eb82f602238b7737bce3c5e
RARES129 <dascaluraresm@gmail.com>
2023-04-30 09:14:52 +0300

    prettier

--- Code/town-of-salem/src/components/Lobby.js
+++ Code/town-of-salem/src/components/Lobby.js
@@ -1,77 +1,84 @@
-import styles from '../css/lobby.module.css';
+import styles from "../css/lobby.module.css";
 import { useState } from "react";
 
-async function postData(url, data)
-{
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: {
-                "Content-Type": "application/json"
+async function postData(url, data) {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function handleChooseClick(target, setName, setPressed) {
+  let userName = document.getElementById(styles.name).value;
+
+  if (userName.length > 0) {
+    let data = {
+      userId: "",
+    };
+    data.userId = userName;
+
+    await postData("http://localhost:3000/lobbies", data);
+
+    setName(userName);
+
+    setPressed(true);
+  }
+}
+
+async function handleStartClick(pressed) {
+  if (pressed) {
+    let data = {
+      start: true,
+    };
+
+    await postData("http://localhost:3000/state/0/start_game", data);
+  }
+}
+
+function Lobby(props) {
+  const [pressed, setPressed] = useState(false);
+
+  return (
+    <div className={styles.center}>
+      <div className={styles.formContainer}>
+        <h1>PLEASE CHOOSE A USERNAME</h1>
+        <input
+          type="text"
+          minLength="1"
+          maxLength="15"
+          id={styles.name}
+        ></input>
+        <div className={styles.buttonBackgroundLobby}>
+          <button
+            onClick={(e) =>
+              handleChooseClick(e.target, props.setName, setPressed)
             }
-        });
-    } catch ( error ) {
-        console.log(error);
-    }
-}
-
-async function handleChooseClick(target, setName, setPressed)
-{
-    let userName = document.getElementById(styles.name).value;
-
-    if (userName.length > 0)
-    {
-        let data = {
-            userId: ""
-        };
-        data.userId = userName;
-
-        await postData("http://localhost:3000/lobbies", data);
-
-        setName(userName);
-
-        setPressed(true);
-    }
-}
-
-async function handleStartClick(pressed)
-{
-    if (pressed)
-    {
-        let data = {
-            start: true
-        };
-
-        await postData("http://localhost:3000/state/0/start_game", data);
-    }
-}
-
-function Lobby(props)
-{
-    const [pressed, setPressed] = useState(false);
-
-    return (
-        <div className={styles.center}>
-        <div className={styles.formContainer}>
-            <h1>PLEASE CHOOSE A USERNAME</h1>
-            <input type="text" minLength="1" maxLength="15" id={styles.name}></input>
-            <div className={styles.buttonBackgroundLobby}>
-                <button 
-                    onClick={e => handleChooseClick(e.target, props.setName, setPressed)}
-                    id={styles.choose} className={styles.buttonLobby}>CHOOSE
-                </button>
-            </div>
-            <h1>WHEN YOU ARE READY PRESS START TO START THE GAME</h1>
-            <div className={styles.buttonBackgroundLobby}>
-                <button 
-                    onClick={e => handleStartClick(pressed)}
-                    id={styles.start} className={styles.buttonLobby}>START
-                </button>
-            </div>
+            id={styles.choose}
+            className={styles.buttonLobby}
+          >
+            CHOOSE
+          </button>
         </div>
+        <h1>WHEN YOU ARE READY PRESS START TO START THE GAME</h1>
+        <div className={styles.buttonBackgroundLobby}>
+          <button
+            onClick={(e) => handleStartClick(pressed)}
+            id={styles.start}
+            className={styles.buttonLobby}
+          >
+            START
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Lobby;
--- Code/town-of-salem/src/components/UserList.js
+++ Code/town-of-salem/src/components/UserList.js
@@ -1,36 +1,42 @@
-import User from './User.js';
-import styles from '../css/users.module.css';
+import User from "./User.js";
+import styles from "../css/users.module.css";
 
-function handleCloseWillClick()
-{
-    let willElement = document.getElementById(styles.willContainer);
-    willElement.style.display = "none";
+function handleCloseWillClick() {
+  let willElement = document.getElementById(styles.willContainer);
+  willElement.style.display = "none";
 }
 
-function UserList(props)
-{
-    let usersList = props.usersList;
-    return (
-        <div className={styles.usersWillContainer}>
-            <div className={styles.usersListContainerDiv}>
-                <h3 id={styles.playersTxt}>Players List</h3>
-                <div className={styles.usersListContainer}>
-                    {usersList.map((user) => <User key={user.id} user={user} gameState={props.gameState} currentUser={props.currentUser} />)}
-                </div>
-            </div>
-            <div id={styles.willContainer} style={{display: "none"}}>
-                <div className={styles.uiWillContainer}>
-                    <input 
-                        onClick={e => handleCloseWillClick()}
-                        id={styles.closeWill} type="image" src='/media/close.png'>
-                    </input>
-                    <h3 id={styles.willTxt}>User will</h3>
-                </div>
-                <div id={styles.userWill}>This is the will of user.</div>
-            </div>
+function UserList(props) {
+  let usersList = props.usersList;
+  return (
+    <div className={styles.usersWillContainer}>
+      <div className={styles.usersListContainerDiv}>
+        <h3 id={styles.playersTxt}>Players List</h3>
+        <div className={styles.usersListContainer}>
+          {usersList.map((user) => (
+            <User
+              key={user.id}
+              user={user}
+              gameState={props.gameState}
+              currentUser={props.currentUser}
+            />
+          ))}
         </div>
-    );
+      </div>
+      <div id={styles.willContainer} style={{ display: "none" }}>
+        <div className={styles.uiWillContainer}>
+          <input
+            onClick={(e) => handleCloseWillClick()}
+            id={styles.closeWill}
+            type="image"
+            src="/media/close.png"
+          ></input>
+          <h3 id={styles.willTxt}>User will</h3>
+        </div>
+        <div id={styles.userWill}>This is the will of user.</div>
+      </div>
+    </div>
+  );
 }
 
 export default UserList;
-
